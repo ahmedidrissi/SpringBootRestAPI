a3311252a91650c0f6c3c3179dcfeebdce005696
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-07-12 20:08:30 +0100

    add JsonProperty annotation

--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -2,12 +2,17 @@ package model;
 
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Person {
 
     private final UUID id;
     private final String name;
     
-    public Person(UUID id, String name) {
+    public Person(
+        @JsonProperty("id") UUID id,
+        @JsonProperty("name") String name
+        ) {
         this.id = id;
         this.name = name;
     }
