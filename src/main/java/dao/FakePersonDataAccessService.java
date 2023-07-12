68f209bcec35173ea927c1c65a044f7fbd93bd78
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-07-12 19:57:54 +0100

    create fake person data access service

---
+++ src/main/java/dao/FakePersonDataAccessService.java
@@ -0,0 +1,22 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Repository;
+
+import model.Person;
+
+@Repository("fakeDao")
+public class FakePersonDataAccessService implements PersonDao {
+
+    private static List<Person> DB = new ArrayList<>();
+    
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        DB.add(new Person(id, person.getName()));
+        return 1;
+    }
+    
+}
