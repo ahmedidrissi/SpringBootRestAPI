4672132910cd089f4199e8dfe431b6029d382d79
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-07-12 19:58:05 +0100

    create person controller

---
+++ src/main/java/api/PersonController.java
@@ -0,0 +1,24 @@
+package api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import model.Person;
+import service.PersonService;
+
+@RestController
+public class PersonController {
+    
+    private final PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping
+    public void addPerson(Person person) {
+        personService.addPerson(person);
+    }
+}
