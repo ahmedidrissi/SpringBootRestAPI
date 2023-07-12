cc3758919100ad7292a68cbc445b08747a5ce8e3
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-07-12 19:57:09 +0100

    create person model

---
+++ src/main/java/model/Person.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.util.UUID;
+
+public class Person {
+
+    private final UUID id;
+    private final String name;
+    
+    public Person(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public UUID getId() { return id; }
+
+    public String getName() { return name; }
+    
+}
